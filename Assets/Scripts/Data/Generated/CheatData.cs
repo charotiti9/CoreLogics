44de9a8913273fb9c5c8f17e6ebf547493129858
charotiti9 <charotiti9@gmail.com>
2026-01-17 22:09:13 +0900

    Cheat 시트데이터 추가

--- Assets/Scripts/Data/Generated/CheatData.cs
+++ Assets/Scripts/Data/Generated/CheatData.cs
@@ -1,4 +1,4 @@
-// Auto-Generated from Cheat_Schema.csv
+// Auto-Generated from CheatData_Schema.csv
 // 수정하지 마세요!
 
 using System;
@@ -6,13 +6,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
-[CSVTable("Cheat")]
-public class Cheat : ICSVData
+[CSVTable("CheatData")]
+public class CheatData : ICSVData
 {
     /// <summary>
     /// 치트 명령어 ID (ICheat 구현 클래스명과 동일)
     /// </summary>
     public string ID;
+
+    /// <summary>
+    /// 치트 설명
+    /// </summary>
+    public string Description;
 
     /// <summary>
     /// 매개변수 정의 (name:type|name:type|...)
